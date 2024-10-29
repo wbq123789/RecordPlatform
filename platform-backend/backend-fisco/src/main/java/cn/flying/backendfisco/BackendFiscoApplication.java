50d99ed8915fabb3228c5886e9a66e7d736cce56
flying <1724593655@qq.com>
2024-10-29 11:25:42 +0800

    init

---
+++ platform-backend/backend-fisco/src/main/java/cn/flying/backendfisco/BackendFiscoApplication.java
@@ -0,0 +1,13 @@
+package cn.flying.backendfisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFiscoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFiscoApplication.class, args);
+    }
+
+}
